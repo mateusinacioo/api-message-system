ef375aff7e35f5cf0eecbbb6eae2f619db25170b
mateusinacioo <mateus.inacio321@gmail.com>
2024-11-29 21:03:46 -0300

    ajustes nas views e controller session user

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,7 +3,7 @@ class Users::SessionsController < Devise::SessionsController
     user = User.find_by(email: params[:user][:email])
 
     if user && !user.status 
-      redirect_to new_user_session_path
+      redirect_to new_user_session_path, alert: 'Sua conta está inativa.'
     else
       super
     end
